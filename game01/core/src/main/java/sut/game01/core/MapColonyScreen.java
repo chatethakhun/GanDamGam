6b707e26d93f9aac118d48b9f567ac46e73e5a6c
Chatethakhun Khongthon <chatethakhun.jack@hotmail.com>
2016-03-26 00:58:17 +0700

    Edit Map Screen

--- game01/core/src/main/java/sut/game01/core/MapColonyScreen.java
+++ game01/core/src/main/java/sut/game01/core/MapColonyScreen.java
@@ -3,6 +3,7 @@ package sut.game01.core;
 import playn.core.Image;
 import playn.core.ImageLayer;
 import playn.core.Mouse;
+import playn.core.util.Clock;
 import tripleplay.game.Screen;
 import tripleplay.game.ScreenStack;
 
@@ -14,18 +15,55 @@ import static playn.core.PlayN.*;
 public class MapColonyScreen extends Screen {
 
     private final ScreenStack ss;
+    private final Image bgImage;
     private final ImageLayer bgLayer;
     private final ImageLayer backarrow;
+    private final Image cloudImage;
+    private final ImageLayer mainLayer;
+    private final ImageLayer cloudLayer;
+    private final ImageLayer cloudLayer2;
+    private final ImageLayer level4;
+    private final ImageLayer level5;
+    private final ImageLayer level6;
+    private float x = 10;
+    private float y = 100;
+    private float a = 10;
+    private float b = 250;
+    private int i;
+
+
+
 
     public MapColonyScreen(final ScreenStack ss) {
         this.ss = ss;
 
-        Image bgImage = assets().getImage("images/colony.png");
+        this.bgImage = assets().getImage("images/colony.png");
         this.bgLayer = graphics().createImageLayer(bgImage);
 
         Image bacArrowImage = assets().getImage("images/backarrow.png");
         this.backarrow = graphics().createImageLayer(bacArrowImage);
         backarrow.setTranslation(15, 225);
+
+        cloudImage = assets().getImage("images/cloud.png");
+        cloudLayer = graphics().createImageLayer(cloudImage);
+        cloudLayer2 = graphics().createImageLayer(cloudImage);
+
+        Image mainImage = assets().getImage("images/main.png");
+        this.mainLayer = graphics().createImageLayer(mainImage);
+        mainLayer.setTranslation(15, 15);
+
+        Image level4Image = assets().getImage("images/l4.png");
+        this.level4 = graphics().createImageLayer(level4Image);
+        level4.setTranslation(150, 350);
+
+        Image level5Image = assets().getImage("images/l5.png");
+        this.level5 = graphics().createImageLayer(level5Image);
+        level5.setTranslation(400, 300);
+
+        Image level6Image = assets().getImage("images/l6.png");
+        this.level6 = graphics().createImageLayer(level6Image);
+        level6.setTranslation(380, 80);
+
 
         backarrow.addListener(new Mouse.LayerAdapter() {
             @Override
@@ -34,16 +72,49 @@ public class MapColonyScreen extends Screen {
             }
         });
 
+        mainLayer.addListener(new Mouse.LayerAdapter() {
+            @Override
+            public void onMouseUp(Mouse.ButtonEvent event) {
+                for (i = 0; i <= ss.size(); i++){
+                    ss.remove(ss.top());
+                }
+            }
+        });
+
 
 
     }
+
+    @Override
+    public void update(int delta) {
+
+        x += 0.1f * delta;
+        if ( x > this.bgImage.width() + this.cloudImage.width()){
+            x = -cloudImage.width();
+        }
+        cloudLayer.setTranslation(x, y);
+
+        a += 0.3f * delta/2;
+        if ( a > this.bgImage.width() - this.cloudImage.width()){
+            a = -cloudImage.width();
+        }
+        cloudLayer2.setTranslation(a, b);
+    }
+
+
 
     @Override
     public void wasShown() {
         super.wasShown();
 
         this.layer.add(bgLayer);
+        this.layer.add(mainLayer);
         this.layer.add(backarrow);
+        this.layer.add(cloudLayer);
+        this.layer.add(cloudLayer2);
+        this.layer.add(level4);
+        this.layer.add(level5);
+        this.layer.add(level6);
 
 
     }
--- game01/core/src/main/java/sut/game01/core/MapSpaceScreen.java
+++ game01/core/src/main/java/sut/game01/core/MapSpaceScreen.java
@@ -18,10 +18,17 @@ public class MapSpaceScreen extends Screen {
     private final ImageLayer bgLayer;
     private final ImageLayer mainLayer;
     private final ImageLayer arrowLayer;
+    private final ImageLayer planeLayer;
+    private final ImageLayer levev1;
+    private final ImageLayer levev2;
+    private final ImageLayer levev3;
+    private int i;
+
 
 
     public MapSpaceScreen(final ScreenStack ss) {
         this.ss = ss;
+
         this.mapColonyScreen = new MapColonyScreen(ss);
 
 
@@ -36,11 +43,29 @@ public class MapSpaceScreen extends Screen {
         this.arrowLayer = graphics().createImageLayer(arrowImage);
         arrowLayer.setTranslation(570, 225);
 
+        Image planeImage = assets().getImage("images/plane.png");
+        this.planeLayer = graphics().createImageLayer(planeImage);
+        planeLayer.setTranslation(100, 200);
+
+
+        Image level1Image = assets().getImage("images/l1.png");
+        this.levev1 = graphics().createImageLayer(level1Image);
+        levev1.setTranslation(150, 350);
+
+        Image level2Image = assets().getImage("images/l2.png");
+        this.levev2 = graphics().createImageLayer(level2Image);
+        levev2.setTranslation(200, 150);
+
+        Image level3Image = assets().getImage("images/l3.png");
+        this.levev3 = graphics().createImageLayer(level3Image);
+        levev3.setTranslation(380, 80);
+
         this.mainLayer.addListener(new Mouse.LayerAdapter() {
             @Override
             public void onMouseDown(Mouse.ButtonEvent event) {
-                ss.remove(ss.top());
-                ss.remove(ss.top());
+                for (i = 0;i <= ss.size(); i++){
+                    ss.remove(ss.top());
+                }
             }
         });
 
@@ -59,5 +84,9 @@ public class MapSpaceScreen extends Screen {
         this.layer.add(bgLayer);
         this.layer.add(mainLayer);
         this.layer.add(arrowLayer);
+        this.layer.add(planeLayer);
+        this.layer.add(levev1);
+        this.layer.add(levev2);
+        this.layer.add(levev3);
     }
 }
