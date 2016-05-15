3fdb281e173ddd181838cb9a9113b699a5944217
Chatethakhun Khongthon <chatethakhun.jack@hotmail.com>
2016-05-15 20:57:05 +0700

    add Physics

--- game01/core/src/main/java/sut/game01/core/GamePlay.java
+++ game01/core/src/main/java/sut/game01/core/GamePlay.java
@@ -1,14 +1,22 @@
 package sut.game01.core;
 
-import playn.core.Image;
-import playn.core.ImageLayer;
-import playn.core.Mouse;
+import org.jbox2d.callbacks.DebugDraw;
+import org.jbox2d.collision.shapes.EdgeShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.World;
+import playn.core.*;
+import playn.core.util.Clock;
 import sut.game01.core.Characters.SandRock;
 import tripleplay.game.Screen;
 import tripleplay.game.ScreenStack;
 
+import java.util.ArrayList;
+
 import static playn.core.PlayN.assets;
 import static playn.core.PlayN.graphics;
+import static playn.core.PlayN.mouse;
 
 /**
  * Created by Chatethakhun on 24/3/2559.
@@ -18,8 +26,15 @@ public class GamePlay extends Screen {
     private final ScreenStack ss;
     private final ImageLayer bgLayer;
     private final ImageLayer backButton;
-    private int i;
-    private SandRock sandRock = new SandRock(560f,400f);
+    private int p;
+    private ArrayList<SandRock> sandRock = new ArrayList<SandRock>();
+    private boolean showDebugDraw = true;
+    private World world;
+    private DebugDrawBox2D debugDraw;
+    public static float M_PER_PIXEL = 1 / 26.666667f;
+    private static int width = 24;
+    private static int height = 18;
+    private int i=0,j=0;
 
     public GamePlay(final ScreenStack ss) {
         this.ss = ss;
@@ -35,11 +50,19 @@ public class GamePlay extends Screen {
         backButton.addListener(new Mouse.LayerAdapter(){
 
             public void onMouseDown(Mouse.ButtonEvent event) {
-                for (i = 0; i < ss.size(); i++){
+                for (p = 0; p < ss.size(); p++){
                     ss.remove(ss.top());
                 }
             }
         });
+
+
+        Vec2 gravity = new Vec2(0.0f, 10.0f);
+        world = new World(gravity);
+        world.setWarmStarting(true);
+        world.setAutoClearForces(true);
+
+
 
     }
 
@@ -48,13 +71,69 @@ public class GamePlay extends Screen {
         super.wasShown();
         this.layer.add(bgLayer);
         this.layer.add(backButton);
-        this.layer.add(sandRock.layer());
+        sandRock.add(i,new SandRock(world,350f,0f));
+
+
+        /*mouse().setListener(new Mouse.Adapter() {
+            @Override
+            public void onMouseDown(Mouse.ButtonEvent event) {
+                sandRock.add(i,new SandRock(world,event.x(),event.y()));
+                layer.add(sandRock.get(i).layer());
+                i++;
+                j++;
+            }
+
+        });
+*/
+
+
+        Body ground = world.createBody(new BodyDef());
+        EdgeShape groundShape = new EdgeShape();
+        groundShape.set(new Vec2( 5, height ), new Vec2(width, height));
+        ground.createFixture(groundShape, 0.0f);
+        layer.add(sandRock.get(j).layer());
+
+        if (showDebugDraw) {
+            CanvasImage image = graphics().createImage((int) (width / GamePlay.M_PER_PIXEL),
+                    (int) (height / GamePlay.M_PER_PIXEL));
+            layer.add(graphics().createImageLayer(image));
+            debugDraw = new DebugDrawBox2D();
+            debugDraw.setCanvas(image);
+            debugDraw.setFlipY(false);
+            debugDraw.setStrokeAlpha(150);
+            debugDraw.setFillAlpha(75);
+            debugDraw.setStrokeWidth(2.0f);
+            debugDraw.setFlags(DebugDraw.e_shapeBit |
+                    DebugDraw.e_jointBit |
+                    DebugDraw.e_aabbBit);
+            debugDraw.setCamera(0, 0, 1f / GamePlay.M_PER_PIXEL);
+            world.setDebugDraw(debugDraw);
+
+
+        }
+
     }
 
     @Override
     public void update(int delta) {
         super.update(delta);
-        sandRock.update(delta);
+        world.step(0.033f, 10, 10);
+        for(int k=0;k<=j;k++){
+            sandRock.get(k).update(delta);
+        }
+    }
 
+
+
+    @Override
+    public void paint(Clock clock) {
+        super.paint(clock);
+        for(int k=0;k<=j;k++){
+            sandRock.get(k).paint(clock);
+        }
+        if(showDebugDraw) {
+            debugDraw.getCanvas().clear();
+            world.drawDebugData();
+        }
     }
 }
